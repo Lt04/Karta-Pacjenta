5c9384f22ae9cafbd3d90d63da2e90067bfd0c3b
Lt04 <jakub.k.borowiak@student.put.poznan.pl>
2020-06-04 18:45:07 +0200

    Lista pacjentów i szczegóły

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var server          = require('http').Server(options, app);
 const bodyParser    = require('body-parser');
 var mkFhir = require('fhir.js');
 var client = mkFhir({
-    baseUrl: 'http://hapi.fhir.org/baseR4/Patient?_pretty=true'
+    baseUrl: 'http://hapi.fhir.org/baseR4'
 });
 
 app.use(express.static('public'));
@@ -18,28 +18,123 @@ app.use(function(req, res, next) {
     next();
 });
 
+var bundle
+
 server.listen(80)
 
 app.post('/showpatlist', function(request, response){
-    client
-    .search( {type: 'Patient', query: { 'birthdate': '1974' }})
-    .then(function(res){
-        var bundle = res.data;
-        console.log(bundle.entry[0].resource.birthDate)
-        var count = (bundle.entry && bundle.entry.length) || 0;
-        console.log("# Patients born in 1974: ", count);
-    })
-    .catch(function(res){
-        // Error responses
-        if (res.status){
-            console.log('Error', res.status);
-        }
+    var start = request.body.page*10
+    var stop = start + 10
+    if(request.body.name == ""){
+        var searchPatients = client
+        .search( {type: 'Patient', query: { }})
+        .then(function(res) {
+            bundle = res.data
+            if(start >= res.data.entry.length || start < 0){
+                response.send(403)
+                return
+            }
+            var html_resp = "<table><tr><th>Given name</th><th>Family name</th><th>Details</th></tr>"
+            for(var i = start; i < stop; i++){
+                if(res.data.entry[i] == undefined){
+                    break
+                }
+                html_resp += "<tr><td>" + res.data.entry[i].resource.name[0].given[0] + "</td><td>" + res.data.entry[i].resource.name[0].family + "</td>" + 
+                "<td><button onclick=details('" + res.data.entry[i].resource.id + "') type='button' class='btn btn-light'>Details</button></td></tr>"
+            }
+            html_resp += "</table>"
+            response.send(200, html_resp)
+            return
+        })
+    }
+    else{
+        var searchPatients = client
+        .search( {type: 'Patient', query: {family: request.body.name }})
+        .then(function(res) {
+            if(start >= res.data.entry.length || start < 0){
+                response.send(403)
+                return
+            }
+            var html_resp = "<table><tr><th>Given name</th><th>Family name</th><th>Details</th></tr>"
+            for(var i = start; i < stop; i++){
+                if(res.data.entry[i] == undefined){
+                    break
+                }
+                html_resp += "<tr><td>" + res.data.entry[i].resource.name[0].given[0] + "</td><td>" + res.data.entry[i].resource.name[0].family + "</td>" + 
+                "<td><button onclick=details('" + res.data.entry[i].resource.id + "') type='button' class='btn btn-light'>Details</button></td></tr>"
+            }
+            html_resp += "</table>"
+            response.send(200, html_resp)
+            return
+        })
+    }
+})
 
-        // Errors
-        if (res.message){
-            console.log('Error', res.message);
+app.post('/patdet', function(request, response){
+    for(var i = 0; i<bundle.entry.length; i++){
+        if(bundle.entry[i].resource.id == request.body.id){
+            var html_resp = ""
+            html_resp += "<table><tr><td>Family name: </td><td>" + bundle.entry[i].resource.name[0].family + "</td></tr>"
+            html_resp += "<tr><td>Given name: </td><td>" + bundle.entry[i].resource.name[0].given[0] + "</td></tr>"
+            html_resp += "<tr><td>Birth date: </td><td>" + bundle.entry[i].resource.birthDate + "</td></tr>"
+            html_resp += "<tr><td>Gender: </td><td>" + bundle.entry[i].resource.gender + "</td></tr>"
+            if(bundle.entry[i].resource.address == undefined){
+                html_resp += "<tr><td>City: </td><td>" + "unknown" + "</td></tr>"
+                html_resp += "<tr><td>State: </td><td>" + "unknown" + "</td></tr>"
+                html_resp += "<tr><td>Postal code: </td><td>" + "unknown" + "</td></tr>"
+            }
+            else{
+                html_resp += "<tr><td>City: </td><td>" + bundle.entry[i].resource.address[0].city+ "</td></tr>"
+                html_resp += "<tr><td>State: </td><td>" + bundle.entry[i].resource.address[0].state+ "</td></tr>"
+                html_resp += "<tr><td>Postal code: </td><td>" + bundle.entry[i].resource.address[0].postalCode+ "</td></tr>"
+            }
+            if(bundle.entry[i].resource.generalPractitioner == undefined){
+                html_resp += "<tr><td>General Practicioner: </td><td>" + "unknown" + "</td></tr>"
+            }
+            else{
+                html_resp += "<tr><td>General Practicioner: </td><td>" + bundle.entry[i].resource.generalPractitioner[0].display+ "</td></tr>"
+            }
+            html_resp += "<tr><td>Deceased time: </td><td>" + bundle.entry[i].resource.deceasedDateTime+ "</td></tr>"
+            if(bundle.entry[i].resource.communication == undefined){
+                html_resp += "<tr><td>Language: </td><td>" + "unknown" + "</td></tr>"
+            }
+            else{
+                html_resp += "<tr><td>Language: </td><td>" + bundle.entry[i].resource.communication[0].language.text+ "</td></tr>"
+            }
+            if(bundle.entry[i].resource.telecom != undefined){
+                for(var j = 0; j < bundle.entry[i].resource.telecom.length; j++){
+                    html_resp += "<tr><td>" + bundle.entry[i].resource.telecom[j].system + "</td><td>" + bundle.entry[i].resource.telecom[j].value + "</td></tr>"
+                }
+            }
+            html_resp += "</table>"
+            break
         }
-    });
-    response.send(200)
+    }
+    response.send(200, html_resp)
     return
 })
+
+/*function handlePatientsBundle(patientsBundle) {
+    for(var ent of patientsBundle.entry) {
+        var patientName = ent.resource.name;
+        var Birth = ent.resource.birthDate
+        for(var nm of patientName) {
+            //console.log(nm.family + " " + nm.given);
+        }
+        //console.log('########');
+    }
+    //console.log('********************');
+    var hasNext = false;
+    for(var link of patientsBundle.link) {
+        if(link.relation == "next") {
+            hasNext = true;
+            break;
+        }
+    }
+    if(hasNext) {
+        client.nextPage({bundle: patientsBundle})
+            .then(function(res) {
+                handlePatientsBundle(res.data);
+            })
+    }
+}*/
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,58 @@
+var page = 0
 $(function(){
     $("#Startowy").show()
 })
 
-var page = 0
-
 function ShowPatList(){
     axios.post('/showpatlist',{
+        page: page,
+        name: $("#lastname").val()
     })
     .then(function(response){
+        $("#starterr").html(response.data)
+    })
+}
 
+function NextPage(){
+    page += 1
+    axios.post('/showpatlist',{
+        page: page,
+        name: $("#lastname").val()
+    })
+    .then(function(response){
+        $("#starterr").html(response.data)
     })
     .catch(function(error){
-
+        page -= 1
     })
+}
+
+function PrevPage(){
+    page -= 1
+    axios.post('/showpatlist',{
+        page: page,
+        name: $("#lastname").val()
+    })
+    .then(function(response){
+        $("#starterr").html(response.data)
+    })
+    .catch(function(error){
+        page += 1
+    })
+}
+
+function details(id){
+    $("#PatDetails").show()
+    $("#Startowy").hide()
+    axios.post('/patdet', {
+        id: id
+    })
+    .then(function(response){
+        $("#patdet").html(response.data)
+    })
+}
+
+function DetailsHide(){
+    $("#PatDetails").hide()
+    $("#Startowy").show()
 }
